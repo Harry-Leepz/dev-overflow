37fa0ebc0e2f9d76f406f24884a0f159034c3586
Harry Dhillon <hdhillon478@gmail.com>
2025-10-26 18:10:46 +0000

    refactor: update RootLayout to accept children as props

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+type RootLayoutProps = {
+  children: React.ReactNode;
+};
 
-export default function RootLayout() {
-  return <div>Root Layout</div>;
+export default function RootLayout({ children }: RootLayoutProps) {
+  return <div>{children}</div>;
 }
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <main>
-      <h1 className='text-3xl font-bold underline'>Hello, Next.js!</h1>
+      <h1 className='h1-bold font-space-grotesk'>Hello, Next.js!</h1>
     </main>
   );
 }

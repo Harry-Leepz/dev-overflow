a152b1bf660516af44f8b64429ebb813b8c16388
Harry Dhillon <hdhillon478@gmail.com>
2025-10-29 18:28:39 +0000

    feat: add RightSidebar component to RootLayout for improved navigation

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,6 @@
 import LeftSidebar from "@/components/navigation/left-sidebar";
 import Navbar from "@/components/navigation/navbar";
+import RightSidebar from "@/components/navigation/right-sidebar";
 
 type RootLayoutProps = {
   children: React.ReactNode;
@@ -16,6 +17,8 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-36 max-md:pb-14 sm:px-14'>
           <div className='mx-auto w-full max-w-5xl'>{children}</div>
         </section>
+
+        <RightSidebar />
       </div>
     </main>
   );

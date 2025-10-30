7e40c4fff5a468b3e7b13da66b71e9dbeb669d10
Harry Dhillon <hdhillon478@gmail.com>
2025-10-30 10:55:26 +0000

    feat: add TagCard component for displaying tags with icons and question count

---
+++ components/cards/index.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import { Badge } from "../ui/badge";
+
+import ROUTES from "@/constants/routes";
+import { getDeviconClassName } from "@/lib/utils";
+
+type TagCardProps = {
+  _id: string;
+  name: string;
+  questions: number;
+  showCount?: boolean;
+  compact?: boolean;
+};
+
+export default function TagCard({
+  _id,
+  name,
+  questions,
+  showCount,
+  compact,
+}: TagCardProps) {
+  const iconClassName = getDeviconClassName(name);
+
+  return (
+    <Link href={ROUTES.TAGS(_id)} className='flex justify-between gap-2'>
+      <Badge className='subtle-medium background-light800_dark300 text-dark400_light500 rounded-md border-none px-4 py-2 uppercase'>
+        <div className='flex-center space-x-2'>
+          <i className={`${iconClassName} text-sm`} />
+          <span>{name}</span>
+        </div>
+      </Badge>
+
+      {showCount && (
+        <p className='small-medium text-dark500_light700'>{questions}</p>
+      )}
+    </Link>
+  );
+}

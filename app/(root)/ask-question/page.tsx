6541e91aa2c191654e322d6271bcba0f9ca32838
Harry Dhillon <hdhillon478@gmail.com>
2025-11-08 11:48:27 +0000

    feat: integrate AskQuestion page with QuestionForm component

--- app/(root)/ask-question/page.tsx
+++ app/(root)/ask-question/page.tsx
@@ -1,5 +1,12 @@
-import React from "react";
+import QuestionForm from "@/components/forms/questions-form";
 
 export default function AskQuestion() {
-  return <div>AskQuestion</div>;
+  return (
+    <>
+      <h1 className='h1-bold text-dark100_light900'>Ask a Question</h1>
+      <div className='mt-9'>
+        <QuestionForm />
+      </div>
+    </>
+  );
 }

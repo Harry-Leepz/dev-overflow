8f6f6b0c41e2b98188568adfc0ce073a1eedc78f
Harry Dhillon <hdhillon478@gmail.com>
2025-11-11 05:43:26 +0000

    feat: refactor TagCard component to support remove functionality and improve structure

--- components/cards/tag-card.tsx
+++ components/cards/tag-card.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import Image from "next/image";
+
 import { Badge } from "../ui/badge";
 
 import ROUTES from "@/constants/routes";
@@ -27,18 +29,39 @@ export default function TagCard({
 }: TagCardProps) {
   const iconClassName = getDeviconClassName(name);
 
-  return (
-    <Link href={ROUTES.TAGS(_id)} className='flex justify-between gap-2'>
-      <Badge className='subtle-medium background-light800_dark300 text-dark400_light500 rounded-md border-none px-4 py-2 uppercase'>
+  const content = (
+    <>
+      <Badge className='subtle-medium background-light800_dark300 text-dark400_light500 rounded-md border-none px-4 py-2 uppercase flex flex-row gap-2'>
         <div className='flex-center space-x-2'>
           <i className={`${iconClassName} text-sm`} />
           <span>{name}</span>
         </div>
+
+        {remove && (
+          <Image
+            src='/icons/close.svg'
+            alt='Close icon'
+            width={12}
+            height={12}
+            className='cursor-pointer object-contain invert-0 dark:invert'
+            onClick={handleRemove}
+          />
+        )}
       </Badge>
 
       {showCount && (
         <p className='small-medium text-dark500_light700'>{questions}</p>
       )}
-    </Link>
+    </>
   );
+
+  if (compact) {
+    return isButton ? (
+      <button className='flex justify-between gap-2'>{content}</button>
+    ) : (
+      <Link href={ROUTES.TAGS(_id)} className='flex justify-between gap-2'>
+        {content}
+      </Link>
+    );
+  }
 }

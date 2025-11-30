edde98b965419b35dfc980e9de579e5027b49d36
Harry Dhillon <hdhillon478@gmail.com>
2025-11-30 12:29:23 +0000

    feat: add Vote model with schema definition for MongoDB

---
+++ database/models/vote.model.ts
@@ -0,0 +1,22 @@
+import { model, models, Schema, Types } from "mongoose";
+
+export type TVote = {
+  author: Types.ObjectId;
+  id: Types.ObjectId;
+  type: "question" | "answer";
+  voteType: "upvote" | "downvote";
+};
+
+const VoteSchema = new Schema<TVote>(
+  {
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+    id: { type: Schema.Types.ObjectId, required: true },
+    type: { type: String, enum: ["question", "answer"], required: true },
+    voteType: { type: String, enum: ["upvote", "downvote"], required: true },
+  },
+  { timestamps: true }
+);
+
+const Vote = models.Vote || model<TVote>("Vote", VoteSchema);
+
+export default Vote;

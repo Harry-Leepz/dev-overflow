811b79e87f9365e82e7a3fa33a64561efb8e0277
Harry Dhillon <hdhillon478@gmail.com>
2025-12-09 19:24:03 +0000

    feat: add AccountSchema for account validation

--- lib/validations.ts
+++ lib/validations.ts
@@ -83,3 +83,38 @@ export const UserSchema = z.object({
   portfolio: z.url({ message: "Portfolio must be a valid URL." }).optional(),
   reputation: z.number().optional(),
 });
+
+// Accounts
+export const AccountSchema = z.object({
+  userId: z.string().min(1, { message: "User ID is required." }),
+  name: z.string().min(1, { message: "Name is required." }),
+  image: z.url({ message: "Image must be a valid URL." }).optional(),
+  provider: z.string().min(1, { message: "Provider is required." }),
+  providerAccountId: z
+    .string()
+    .min(1, { message: "Provider Account ID is required." }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long." })
+    .max(100, { message: "Password cannot exceed 100 characters." })
+    .regex(/[A-Z]/, {
+      message: "Password must contain at least one uppercase letter.",
+    })
+    .regex(/[a-z]/, {
+      message: "Password must contain at least one lowercase letter.",
+    })
+    .regex(/[0-9]/, { message: "Password must contain at least one number." })
+    .regex(/[^a-zA-Z0-9]/, {
+      message: "Password must contain at least one special character.",
+    })
+    .optional(),
+});
+
+// export type TAccount = {
+//   userId: Schema.Types.ObjectId;
+//   name: string;
+//   image?: string;
+//   password?: string;
+//   provider: string;
+//   providerAccountId: string;
+// };
